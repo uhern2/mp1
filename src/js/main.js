d27993a5f0102ca5668f3744de2a0969403e130c
uhern2 <uhern2@illinois.edu>
2025-09-17 19:32:50 -0500

    Implemented navbar, scrolling, smooth scrolling, and navbar resizing

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,12 @@
 /* Your JS here. */
 console.log('Hello World!')
+
+
+window.addEventListener("scroll", function(){
+    const navbar = document.getElementById("navbar");
+    if(window.scrollY > 70){
+        navbar.classList.add("shrink");
+    } else {
+        navbar.classList.remove("shrink");
+    }
+});
