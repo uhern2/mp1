de1bd70b858efd2e231cca9e488bf474084afc9b
uhern2 <uhern2@illinois.edu>
2025-09-21 18:09:35 -0500

    Fixed background img, fixed navbar, added a multi column layout, added a modal, added a embedded video

--- src/js/main.js
+++ src/js/main.js
@@ -10,3 +10,26 @@ window.addEventListener("scroll", function(){
         navbar.classList.remove("shrink");
     }
 });
+
+const open = document.querySelectorAll(".openModal");
+const close = document.querySelectorAll(".close");
+
+open.forEach(btn => {
+    btn.addEventListener("click", () => {
+        const ID = btn.getAttribute("data-target");
+        document.getElementById(ID).style.display = "flex";
+    });
+});
+
+
+close.forEach(btn => {
+    btn.addEventListener("click", () => {
+        btn.parentElement.style.display = "none";
+    });
+});
+
+window.addEventListener("click", (e) => {
+    if(e.target.classList.contains("modal")){
+        e.target.style.display = "none"
+    }
+});
